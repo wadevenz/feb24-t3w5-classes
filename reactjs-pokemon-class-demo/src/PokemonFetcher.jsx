b0421ee857d114ac42ebac6253d060bac99072d1
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-14 11:39:17 +1000

    feat: componentDidMount with multiple pokemon data

--- reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -16,16 +16,25 @@ export default class PokemonFetcher extends React.Component {
 
     // runs automtically ONCE when the page first loads
     async componentDidMount(){
-        // generate a random number for random Pokemon number
-        let randomNumber = Math.floor(Math.random() * 1025) + 1;
 
-        // pass random Pokemon number into fetch request
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-        let data = await response.json();
+        for (let index = 0; index < 6; index++) {
+            // generate a random number for random Pokemon number
+            let randomNumber = Math.floor(Math.random() * 1025) + 1;
 
-        // set fetch response data into state
-        this.setState({pokemonList: [data]});
+            // pass random Pokemon number into fetch request
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+            let data = await response.json();
 
+            // set fetch response data into state
+            // this.setState({pokemonList: [data]});
+            // this.setState({pokemonList: [...this.state.pokemonList, data]});
+            this.setState((previousState) => {
+                return {
+                    pokemonList: [...previousState.pokemonList, data]
+                }
+            });
+
+        }
         console.log("PokemonFetcher first load on the page");
     }
 
@@ -43,5 +52,5 @@ export default class PokemonFetcher extends React.Component {
                 </button>
             </div>
         );
-    };
+    }
 }
